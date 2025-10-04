91df44c6ff1230255d33f3afd38ff6c983a1850e
alexanderdombroski <dom21013@byui.edu>
2025-10-04 13:51:32 -0600

    feat(dev): add tslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+	importPlugin.flatConfigs.typescript,
+	{
+		ignores: ['node_modules/**', 'dist/**'],
+	},
+	{
+		files: ['**/*.ts'],
+	},
+	{
+		plugins: {
+			'@typescript-eslint': typescriptEslint,
+		},
+
+		languageOptions: {
+			parser: tsParser,
+			ecmaVersion: 2022,
+			sourceType: 'module',
+		},
+
+		rules: {
+			'@typescript-eslint/naming-convention': [
+				'warn',
+				{
+					selector: 'import',
+					format: ['camelCase', 'PascalCase'],
+				},
+			],
+
+			'import/extensions': [
+				'error',
+				'never',
+				{
+					ts: 'never',
+					js: 'never',
+				},
+			],
+
+			'no-unused-vars': ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+			curly: 'warn',
+			eqeqeq: 'warn',
+			'no-throw-literal': 'warn',
+			semi: 'warn',
+		},
+	},
+];
